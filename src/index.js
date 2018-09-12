140bb7123228e1c8db3fa4d4db63a4f46c625659
Andrey Kashapov <kashapovandrey@gmail.com>
2018-09-12 13:55:12 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-module.exports = function solveEquation(equation) {  
-  var a;
-  var b;
-  var c;
-  var d;
-  var x1;
-  var x2;
+module.exports = function solveEquation(equation) {
+  let a, b, c, d, x1, x2, tmp, res;
 
-  var tmp;
-  var res;
+  tmp = equation.replace(/\ /g, "");
 
-  tmp = equation.replace(/\ /g,"");
-  
   tmp = tmp.split('x');
 
-  for (var i=0; i<tmp.length; i++) {
-    tmp[i] = tmp[i].replace("*","");
-    tmp[i] = tmp[i].replace(/^(.*)-/,"-");
-    tmp[i] = tmp[i].replace(/^(.*)\+/,"");
+  for (let i = 0; i < tmp.length; i++) {
+    tmp[i] = tmp[i].replace("*", "");
+    tmp[i] = tmp[i].replace(/^(.*)-/, "-");
+    tmp[i] = tmp[i].replace(/^(.*)\+/, "");
     //console.log(tmp[i]);
   }
 
@@ -24,25 +16,30 @@ module.exports = function solveEquation(equation) {
   b = +tmp[1];
   c = +tmp[2];
 
-  d = Math.pow(b,2) - 4*a*c;
+  if (a && b && c) {
 
-  if(d > 0) {
-    x1 = Math.round((-1*b + Math.sqrt(d))/(2*a));
-    x2 = Math.round((-1*b - Math.sqrt(d))/(2*a));
+    d = Math.pow(b, 2) - 4 * a * c;
+
+    if (d > 0) {
+      x1 = Math.round((-1 * b + Math.sqrt(d)) / (2 * a));
+      x2 = Math.round((-1 * b - Math.sqrt(d)) / (2 * a));
+
+      res = [x1, x2];
+
+      res.sort(function (a, b) {
+        if (a > b) return 1;
+        if (a < b) return -1;
+        return 0;
+      });
+
+      //console.log(res);
+      return res;
+    }
   }
 
   //console.log(equation);
   //console.log(a,b,c,d,x1,x2);
 
-  res = [x1,x2];
 
-
-  res.sort(function (a, b) {
-    if (a > b) return 1;
-    if (a < b) return -1;
-    return 0;
-  });
-  //console.log(res);
-  return res;
-
+  return null;
 }
